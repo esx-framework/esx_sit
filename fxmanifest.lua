d68e623f2791adb87eedf0ee5531985e2f6defe3
P Gergő <69343477+Rav3n95@users.noreply.github.com>
2023-01-18 14:45:38 +0100

    feat - new repos

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Sit'
+lua54 'yes'
+version '1.0'
+legacyversion '1.9.1'
+
+shared_script '@es_extended/imports.lua'
+
+server_scripts {
+	'config.lua',
+	'lists/seat.lua',
+	'server.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'lists/seat.lua',
+	'client.lua'
+}
